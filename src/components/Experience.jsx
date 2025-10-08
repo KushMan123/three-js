e029dcecb7e0cee49f7a0cde76f7820ae18778a4
KushMan123 <57740881+KushMan123@users.noreply.github.com>
2025-10-08 17:56:47 +0545

    Before adding leva

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,19 @@
-import { Float, Line, OrbitControls } from "@react-three/drei";
+import {
+  Float,
+  Line,
+  OrbitControls,
+  PerspectiveCamera,
+  useScroll,
+} from "@react-three/drei";
 import { Spaceship } from "./Spaceship";
 import { Background } from "./Background";
 import { useMemo, useRef } from "react";
 
 import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
 
 export const Experience = () => {
-  const LINE_NO_POINTS = 42;
+  const LINE_NO_POINTS = 2000;
 
   const curve = useMemo(() => {
     return new THREE.CatmullRomCurve3(
@@ -29,10 +36,35 @@ export const Experience = () => {
     );
   }, []);
 
+  const shape = useMemo(() => {
+    const shape = new THREE.Shape();
+    shape.moveTo(0, -0.2);
+    shape.lineTo(0, 0.2);
+
+    return shape;
+  }, [curve]);
+
   const linePoints = useMemo(() => {
     return curve.getPoints(LINE_NO_POINTS);
   }, [curve]);
 
+  /** --------------------------Scroll Animations-------------------------------------*/
+  const cameraGroup = useRef();
+  const scroll = useScroll();
+
+  useFrame((_state, delta) => {
+    const curPointIndex = Math.min(
+      Math.round(scroll.offset * linePoints.length),
+      linePoints.length - 1
+    );
+    const curPoint = linePoints[curPointIndex];
+    const pointAhead =
+      linePoints[(Math.min(curPointIndex + 1), linePoints.length - 1)];
+
+    cameraGroup.current.position.lerp(curPoint, delta * 24);
+  });
+
+  /** -------------------------------------------------------------------------------*/
   const backgroundColors = useRef({
     colorA: "#7ea1da",
     colorB: "#abaadd",
@@ -40,8 +72,11 @@ export const Experience = () => {
 
   return (
     <>
-      <OrbitControls />
-      <Background backgroundColors={backgroundColors} />
+      {/* <OrbitControls enableZoom={false} /> */}
+      <group ref={cameraGroup}>
+        <Background backgroundColors={backgroundColors} />
+        <PerspectiveCamera position={[0, 0, 50]} fov={30} makeDefault />
+      </group>
       <Float floatIntensity={2} speed={2}>
         <Spaceship
           rotation-y={Math.PI / 2}
@@ -49,14 +84,29 @@ export const Experience = () => {
           position-y={0.1}
         />
       </Float>
-
-      <Line
-        points={linePoints}
-        color={"white"}
-        opacity={0.7}
-        transparent
-        lineWidth={16}
-      />
+      /*LINE*/
+      <group position-y={-10}>
+        {/* <Line
+          points={linePoints}
+          color={"white"}
+          opacity={0.7}
+          transparent
+          lineWidth={16}
+        /> */}
+        <mesh>
+          <extrudeGeometry
+            args={[
+              shape,
+              {
+                steps: LINE_NO_POINTS,
+                bevelEnabled: false,
+                extrudePath: curve,
+              },
+            ]}
+          />
+          <meshStandardMaterial color={"white"} />
+        </mesh>
+      </group>
     </>
   );
 };

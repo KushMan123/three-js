480565db25f4eb9f040562e17db481751b5b7256
KushMan123 <57740881+KushMan123@users.noreply.github.com>
2025-10-18 12:15:19 +0545

    Refactoring the code

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,22 +1,10 @@
-import { Environment, Sphere } from "@react-three/drei";
-import { useFrame, useThree } from "@react-three/fiber";
-import { Gradient, LayerMaterial } from "lamina";
-import { useRef, useEffect } from "react";
-import { DirectionalLightHelper } from "three";
+import { Environment } from "@react-three/drei";
+import { useRef } from "react";
 
 import * as THREE from "three";
 
 const SunLight = () => {
   const lightRef = useRef();
-  const { scene } = useThree();
-
-  useEffect(() => {
-    // Optional: visualize the light direction
-    const helper = new DirectionalLightHelper(lightRef.current, 2);
-    scene.add(helper);
-    return () => scene.remove(helper);
-  }, [scene]);
-
   return (
     <directionalLight
       ref={lightRef}
@@ -31,45 +19,8 @@ const SunLight = () => {
 };
 
 export const Background = ({ hdrPath }) => {
-  // const start = 0.2;
-  // const end = -0.5;
-
-  // const gradientRef = useRef();
-  // const gradientEnvRef = useRef();
-
-  // useFrame(() => {
-  //   gradientRef.current.colorA = new THREE.Color(
-  //     backgroundColors.current.colorA
-  //   );
-  //   gradientRef.current.colorB = new THREE.Color(
-  //     backgroundColors.current.colorB
-  //   );
-  //   gradientEnvRef.current.colorA = new THREE.Color(
-  //     backgroundColors.current.colorA
-  //   );
-  //   gradientEnvRef.current.colorB = new THREE.Color(
-  //     backgroundColors.current.colorB
-  //   );
-  // });
-
   return (
     <>
-      {/* <Sphere scale={[500, 500, 500]} rotation-y={Math.PI / 2}>
-        <LayerMaterial color={"#ffffff"} side={THREE.BackSide}>
-          <Gradient ref={gradientRef} axes={"y"} start={start} end={end} />
-        </LayerMaterial>
-      </Sphere>
-      <Environment resolution={256} frames={Infinity}>
-        <Sphere
-          scale={[100, 100, 100]}
-          rotation-y={Math.PI / 2}
-          rotation-x={Math.PI}
-        >
-          <LayerMaterial color={"#ffffff"} side={THREE.BackSide}>
-            <Gradient ref={gradientEnvRef} axes={"y"} start={start} end={end} />
-          </LayerMaterial>
-        </Sphere>
-      </Environment> */}
       <ambientLight intensity={1} />
       <SunLight />
       <Environment files={hdrPath} background />
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,31 +1,14 @@
-import { OrbitControls, useScroll, Html } from "@react-three/drei";
 import { Spaceship } from "./Spaceship";
 import { Background } from "./Background";
-import { useRef } from "react";
 
 import * as THREE from "three";
-import { useScrollStore } from "../store/scrollStore";
 
 export const Experience = () => {
-  const backgroundColors = useRef({
-    colorA: "#7ea1da",
-    colorB: "#abaadd",
-  });
-
-  // const scroll = useScroll();
-
   return (
     <>
-      {/* <OrbitControls
-        enabled={!isScrolling}
-        enablePan={false}
-        enableZoom={false}
-        makeDefault
-      /> */}
       <group>
         <Background hdrPath="./hdr/HDR_blue_nebulae-1.hdr" />
       </group>
-      {/* <Html>Scroll Offset: {scroll.offset}</Html> */}
       <Spaceship />
     </>
   );
